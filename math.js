cbe4b1bbbc5b636650ea3d878fd5c9fc39252052
faraz-github <farazahmad.email@gmail.com>
2023-06-08 23:26:13 +0530

    Math algos - factorial

--- math.js
+++ math.js
@@ -1,6 +1,6 @@
 // 1. Math  Algorithms
 
-// a) Fibonacci Sequence
+// -------------------------------------------------------------- a) Fibonacci Sequence
 function Fibonacci(n) {
   const fib = [0, 1]; // Initial Numbers
 
@@ -15,9 +15,31 @@ function Fibonacci(n) {
 
 // Testing
 
-console.log(Fibonacci(2));
-console.log(Fibonacci(3));
-console.log(Fibonacci(10));
+// console.log(Fibonacci(2));
+// console.log(Fibonacci(3));
+// console.log(Fibonacci(10));
+
+// Big-O Calculation
+
+// 1. one loop = O(n)
+
+// Hence Big-O for the above program is O(n)
+
+// -------------------------------------------------------------- b) Factorial
+
+function Factorial(n) {
+  let result = 1;
+  for (let index = 2; index <= n; index++) {
+    result = result * index;
+  }
+  return result;
+}
+
+// Testing
+
+console.log(Factorial(1));
+console.log(Factorial(2));
+console.log(Factorial(5));
 
 // Big-O Calculation
 
