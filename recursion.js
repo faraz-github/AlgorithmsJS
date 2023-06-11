f938591aa9a1468d7375c216aed6f49db0775b65
faraz-github <farazahmad.email@gmail.com>
2023-06-11 23:47:50 +0530

    Recursion technique

---
+++ recursion.js
@@ -0,0 +1,8 @@
+// Note: Recursion is a problem solving technique when the function calls itself.
+
+// Common Usecase: When a problem needs to be broken down into smaller problem, recursion technique can be used to simplify the solution.
+
+// Important points
+// 1) A condition to terminate the recursion algorithm.
+// 2) Not always the best, sometimes it can be far worse than the iterative solution.
+// 3) It is not the most straight forward solution.
