94c44f48ccb369e8b47aa3520c8edcd8ae301eef
faraz-github <farazahmad.email@gmail.com>
2023-06-12 23:45:08 +0530

    Recursion algo - fibonacci

--- recursion.js
+++ recursion.js
@@ -6,3 +6,21 @@
 // 1) A condition to terminate the recursion algorithm.
 // 2) Not always the best, sometimes it can be far worse than the iterative solution.
 // 3) It is not the most straight forward solution.
+
+function recursiveFibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+// Testing
+
+console.log(recursiveFibonacci(0)); // 0
+console.log(recursiveFibonacci(1)); // 1
+console.log(recursiveFibonacci(6)); // 8
+
+// Big-O Calculation
+
+// Big-O for the above program is O(2^n)
