25e49bf63ab66593b4f169055d2f3acf1f0e63f7
faraz-github <farazahmad.email@gmail.com>
2023-06-24 23:36:33 +0530

    Cartesian product algo - part 1

---
+++ cartesianProduct.js
@@ -0,0 +1,10 @@
+// What is cartesian product?
+// In mathematics, specifically set theory, the cartesian product of two sets A and B, denoted AxB is the
+// set of all ordered pairs (a,b) where a is in A and b is in B.
+
+// Example:
+// A = [1, 2]
+// B = [3, 4]
+// AxB = [[1,3], [1,4], [2,3], [2, 4]]
+
+// Problem: Given two finite non-empty sets, find their Cartesian Product.
