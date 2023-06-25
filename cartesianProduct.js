d5690ed3dc766d006c3579973263b43f635ffca6
faraz-github <farazahmad.email@gmail.com>
2023-06-25 23:37:56 +0530

    Catesian product algo - part 2

--- cartesianProduct.js
+++ cartesianProduct.js
@@ -8,3 +8,28 @@
 // AxB = [[1,3], [1,4], [2,3], [2, 4]]
 
 // Problem: Given two finite non-empty sets, find their Cartesian Product.
+
+const arr1 = [1, 2];
+const arr2 = [3, 4, 5];
+
+function cartesianProduct(arr1, arr2) {
+  const result = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      result.push([arr1[i], arr2[j]]);
+    }
+  }
+
+  return result;
+}
+
+// Testing
+
+console.log(cartesianProduct(arr1, arr2)); // [[1, 3], [1, 4], [1, 5], [2, 3], [2, 4], [2, 5]]
+
+// Big-O Calculation
+// In this perticular case we have 2 loops but the length of arrays can be diffrent
+// Lets say arr1 is m length and arr2 is n length
+
+// Hence Big-O for the above program is O(mn) is variable length else O(n^2)
